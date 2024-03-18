7b0cefa3a08f0bb2fa0b875963eaf651d5a74063
Arseniy Kotov <arseniyk@amazon.com>
2024-03-18 15:05:41 -0700

    feat(updates): adding data and storage

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,9 +1,29 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { myDemoFunction } from './functions/my-demo-function/resource'
+import { storage } from './storage/resource.js';
+import { data } from './data/resource.js';
 
 
-defineBackend({
+const backend = defineBackend({
+  data,
   auth,
   myDemoFunction,
+  storage
 });
+
+// create the bucket and its stack
+/*const bucketStack = backend.createStack('BucketStack');
+const bucket = new s3.Bucket(bucketStack, 'Bucket', {
+  blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL
+});*/
+
+// allow any authenticated user to read and write to the bucket
+const authRole = backend.auth.resources.authenticatedUserIamRole;
+backend.storage.resources.bucket.grantReadWrite(authRole);
+//bucket.grantReadWrite(authRole);
+
+// allow any guest (unauthenticated) user to read from the bucket
+const unauthRole = backend.auth.resources.unauthenticatedUserIamRole;
+backend.storage.resources.bucket.grantRead(unauthRole);
+//bucket.grantRead(unauthRole);
