8ca469d57b70d1697ba91c1e2e9b5e1c27a5c414
Arseniy Kotov <arseniyk@amazon.com>
2024-04-27 05:51:43 -0700

    fix(deps): update to latest deps

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,91 +1,24 @@
-import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+// amplify/data/resource.ts
 
-/*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rules below
-specify that owners, authenticated via your Auth resource can "create",
-"read", "update", and "delete" their own records. Public users,
-authenticated via an API key, can only "read" records.
-=========================================================================*/
+import { a, defineData, type ClientSchema } from '@aws-amplify/backend';
+
 const schema = a.schema({
   Todo: a
     .model({
       content: a.string(),
+      isDone: a.boolean(),
     })
-    .authorization([a.allow.owner(), a.allow.public().to(['read'])]),
-  AllTypes: a
-    .model({
-      string: a.string(),
-      int: a.integer(),
-      float: a.float(),
-      boolean: a.boolean(),
-      date: a.date(),
-      time: a.time(),
-      datetime: a.datetime(),
-      timestamp: a.timestamp(),
-      email: a.email(),
-      json: a.json(),
-      phone: a.phone(),
-      url: a.url(),
-      ipAddress: a.ipAddress(),
-      nonModel: a.customType({
-        string: a.string(),
-        int: a.integer(),
-        float: a.float(),
-        boolean: a.boolean(),
-        date: a.date(),
-        time: a.time(),
-        datetime: a.datetime(),
-        timestamp: a.timestamp(),
-        email: a.email(),
-        json: a.json(),
-        phone: a.phone(),
-        url: a.url(),
-        ipAddress: a.ipAddress(),
-      }),
-      enum: a.enum(['some', 'enum', 'value']),
-    })
-    .authorization([a.allow.owner(), a.allow.private()]),
+    .authorization((allow) => [allow.publicApiKey()]),
 });
 
+// Used for code completion / highlighting when making requests from frontend
 export type Schema = ClientSchema<typeof schema>;
 
+// defines the data resource to be deployed
 export const data = defineData({
   schema,
   authorizationModes: {
     defaultAuthorizationMode: 'apiKey',
-    // API Key is used for a.allow.public() rules
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
-    },
+    apiKeyAuthorizationMode: { expiresInDays: 30 },
   },
 });
-
-/*== STEP 2 ===============================================================
-Go to your frontend source code. From your client-side code, generate a
-Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
-WORK IN THE FRONTEND CODE FILE.)
-
-Using JavaScript or Next.js React Server Components, Middleware, Server 
-Actions or Pages Router? Review how to generate Data clients for those use
-cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
-=========================================================================*/
-
-/*
-"use client"
-import { generateClient } from "aws-amplify/data";
-import { type Schema } from "@/amplify/data/resource";
-
-const client = generateClient<Schema>() // use this Data client for CRUDL requests
-*/
-
-/*== STEP 3 ===============================================================
-Fetch records from the database and use them in your frontend component.
-(THIS SNIPPET WILL ONLY WORK IN THE FRONTEND CODE FILE.)
-=========================================================================*/
-
-/* For example, in a React component, you can use this snippet in your
-  function's RETURN statement */
-// const { data: todos } = client.models.Todo.list()
-
-// return <ul>{todos.map(todo => <li key={todo.id}>{todo.content}</li>)}</ul>
