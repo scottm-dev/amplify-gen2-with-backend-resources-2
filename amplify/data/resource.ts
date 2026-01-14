314529c4569a3a106c1f09d292836a840bdba390
Scott McDaniel <mcscottm@amazon.com>
2026-01-14 13:21:43 -0800

    add ai

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -3,6 +3,28 @@
 import { a, defineData, type ClientSchema } from '@aws-amplify/backend';
 
 const schema = a.schema({
+
+  chat: a.conversation({
+    aiModel: a.ai.model('Claude 3.5 Haiku'),
+    systemPrompt: 'You are a helpful assistant',
+  })
+    .authorization((allow) => allow.owner()),
+
+  generateRecipe: a.generation({
+    aiModel: a.ai.model('Claude 3.5 Haiku'),
+    systemPrompt: 'You are a helpful assistant that generates recipes.',
+  })
+    .arguments({
+      description: a.string(),
+    })
+    .returns(
+      a.customType({
+        name: a.string(),
+        ingredients: a.string().array(),
+        instructions: a.string(),
+      })
+    )
+    .authorization((allow) => allow.authenticated()),
   Todo: a
     .model({
       content: a.string(),
